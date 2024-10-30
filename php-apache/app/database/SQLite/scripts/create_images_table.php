d06c66b7a7a147a6f402a309bec4cbc6877424c6
AdelRazzok <lhp6.arazzok@gmail.com>
2024-10-30 13:35:02 +0100

    delete: docker php.ini file, update: sqlite tables creation scripts to call the new db classes

--- php-apache/app/database/SQLite/scripts/create_images_table.php
+++ php-apache/app/database/SQLite/scripts/create_images_table.php
@@ -1,19 +1,20 @@
 <?php
 
-try {
-    $pdo = new PDO('sqlite:' . __DIR__ . '/../camagru.db');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+require_once dirname(__DIR__, 3) . '/autoload.php';
 
-    $sql = "CREATE TABLE IF NOT EXISTS images (
-        id INTEGER PRIMARY KEY,
-        file_name TEXT NOT NULL,
-        mime_type TEXT NOT NULL,
-        image_data BLOB NOT NULL,
-        image_size INTEGER NOT NULL,
-        created_at DATETIME NOT NULL,
-        updated_at DATETIME NOT NULL
-    )";
-    $pdo->exec($sql);
-} catch (PDOException $e) {
-    die("Error: " . $e->getMessage());
-}
+use app\database\SQLite\Sqlite;
+
+$db = new Sqlite(dirname(__DIR__) . '/camagru.db');
+$conn = $db->getConnection();
+
+$sql = "CREATE TABLE IF NOT EXISTS images (
+    id INTEGER PRIMARY KEY,
+    file_name TEXT NOT NULL,
+    mime_type TEXT NOT NULL,
+    image_data BLOB NOT NULL,
+    image_size INTEGER NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL
+)";
+
+$conn->exec($sql);
--- php-apache/app/database/SQLite/scripts/create_users_table.php
+++ php-apache/app/database/SQLite/scripts/create_users_table.php
@@ -1,20 +1,21 @@
 <?php
 
-try {
-    $pdo = new PDO('sqlite:' . __DIR__ . '/../camagru.db');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+require_once dirname(__DIR__, 3) . '/autoload.php';
 
-    $sql = 'CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY,
-        email TEXT NOT NULL,
-        username TEXT NOT NULL,
-        password TEXT NOT NULL,
-        email_verified BOOLEAN NOT NULL DEFAULT 0,
-        email_notif_on_comment BOOLEAN NOT NULL DEFAULT 1,
-        created_at DATETIME NOT NULL,
-        updated_at DATETIME NOT NULL
-    )';
-    $pdo->exec($sql);
-} catch (PDOException $e) {
-    die('Error: ' . $e->getMessage());
-}
+use app\database\SQLite\Sqlite;
+
+$db = new Sqlite(dirname(__DIR__) . '/camagru.db');
+$conn = $db->getConnection();
+
+$sql = 'CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    email TEXT NOT NULL,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    email_verified BOOLEAN NOT NULL DEFAULT 0,
+    email_notif_on_comment BOOLEAN NOT NULL DEFAULT 1,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL
+)';
+
+$conn->exec($sql);
