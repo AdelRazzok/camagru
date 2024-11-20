4bb362da7a057e5c684d1ea7cab0f626a517a5f4
AdelRazzok <lhp6.arazzok@gmail.com>
2024-11-20 11:28:50 +0100

    add: router instance and routes declarations

--- php-apache/app/index.php
+++ php-apache/app/index.php
@@ -1,1 +1,15 @@
-<?php phpinfo() ?>
+<?php
+
+require_once __DIR__ . '/autoload.php';
+
+use app\http\Router;
+use app\controllers\HomeController;
+
+session_start();
+
+
+$router = new Router();
+
+$router->get('/', [HomeController::class, 'index']);
+
+$router->run();
