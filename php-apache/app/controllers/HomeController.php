07400651968edb362989f7c70d38a9f112dbcd21
AdelRazzok <lhp6.arazzok@gmail.com>
2024-11-20 11:19:07 +0100

    add: home controller with a test view

---
+++ php-apache/app/controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\controllers;
+
+class HomeController
+{
+    public function index()
+    {
+        echo '
+        <html>
+            <body>
+                <h1>Camagru - Home</h1>
+            </body>
+        </html>';
+    }
+}
