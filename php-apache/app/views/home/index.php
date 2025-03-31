6fa7d50d7902b19fb779fcc8a86dd674880089d4
AdelRazzok <lhp6.arazzok@gmail.com>
2025-03-31 16:37:22 +0200

    update: formatting and links

--- php-apache/app/views/home/index.php
+++ php-apache/app/views/home/index.php
@@ -2,14 +2,14 @@
 
 <div class="md:hidden flex justify-between items-center p-4 shadow-md">
     <h1 class="text-2xl">Camagru</h1>
-    <a href="#" class="bg-sky-500 text-white px-6 py-2 rounded focus:ring-2 focus:ring-blue-300">Login</a>
+    <a href="/login" class="bg-sky-500 text-white px-6 py-2 rounded focus:ring-2 focus:ring-blue-300">Login</a>
 </div>
 
 <main>
-    
+
 </main>
 
 <?php
-    require_once dirname(__DIR__) . '/layouts/navbar.php';
-    require_once dirname(__DIR__) . '/layouts/footer.php'; 
+require_once dirname(__DIR__) . '/layouts/navbar.php';
+require_once dirname(__DIR__) . '/layouts/footer.php';
 ?>
