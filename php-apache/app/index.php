1440874f5e98e1cec133d49cf8daa0e98e256c61
AdelRazzok <lhp6.arazzok@gmail.com>
2025-03-31 16:38:18 +0200

    update: add login route

--- php-apache/app/index.php
+++ php-apache/app/index.php
@@ -3,13 +3,19 @@
 require_once __DIR__ . '/autoload.php';
 
 use http\Router;
+use http\AuthMiddleware;
 use controllers\HomeController;
+use controllers\LoginController;
 
 session_start();
-
 
 $router = new Router();
 
 $router->get('/', [HomeController::class, 'index']);
+$router->get('/login', [LoginController::class, 'index']);
+
+// $router->get('/profile', [ProfileController::class, 'index'], [
+//     'middlewares' => [AuthMiddleware::class]
+// ]);
 
 $router->run();
