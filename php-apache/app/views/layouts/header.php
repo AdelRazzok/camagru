d164cf540f030519741600dff2f14b441fb82391
AdelRazzok <lhp6.arazzok@gmail.com>
2025-04-11 12:17:13 +0200

    update: new font for the app title

--- php-apache/app/views/layouts/header.php
+++ php-apache/app/views/layouts/header.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../../public/favicons/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="../../public/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playwrite+BE+VLG:wght@100..400&family=Playwrite+GB+J+Guides:ital@0;1&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://kit.fontawesome.com/8381830f4b.js" crossorigin="anonymous"></script>
     <title><?= $title ?? 'Camagru' ?></title>
 </head>
+
 <body>
