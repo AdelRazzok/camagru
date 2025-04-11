afe07872a5d774beb358c2d39629ee86991a8ef6
AdelRazzok <lhp6.arazzok@gmail.com>
2025-04-11 12:20:07 +0200

    feature: login page

--- php-apache/app/views/login/index.php
+++ php-apache/app/views/login/index.php
@@ -1,13 +1,33 @@
 <?php require_once dirname(__DIR__) . '/layouts/header.php'; ?>
 
-<div class="md:hidden flex justify-between items-center p-4 shadow-md">
-    <h1 class="text-2xl">Camagru</h1>
-    <a href="/" class="bg-sky-500 text-white px-6 py-2 rounded focus:ring-2 focus:ring-blue-300">Home</a>
-</div>
+<main class="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gray-100">
+    <div class="bg-white rounded-2xl shadow-lg p-8 w-full max-w-md">
+        <h1 class="text-2xl text-center mb-7 playwrite-be-vlg-400">Camagru</h1>
+        <form action="#" method="post" class="flex flex-col space-y-4">
+            <div class="flex flex-col">
+                <label for="username" class="text-sm text-gray-600 font-semibold mb-1">Username</label>
+                <input type="text" name="username" placeholder="john_doe" class="border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-sky-500" required>
+            </div>
 
-<main>
-    <p>Login</p>
+            <div class="flex flex-col">
+                <label for="password" class="text-sm text-gray-600 font-semibold mb-1">Password</label>
+                <input type="password" name="password" placeholder="**********" class="border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-sky-500" required>
+            </div>
+
+            <button type="submit" class="bg-sky-500 text-white py-2 px-4 rounded hover:bg-sky-600 transition duration-200">Login</button>
+        </form>
+
+        <div class="text-center mt-4">
+            <a href="/reset-password" class="text-sm text-blue-600 hover:underline">Forgot password?</a>
+        </div>
+
+        <div class="text-center mt-6 text-sm text-gray-600">
+            <p>Don't have an account? <a href="/signup" class="text-blue-600 hover:underline">Register here</a>.</p>
+        </div>
+    </div>
 </main>
+
+<script src="/public/js/login.js"></script>
 
 <?php
 require_once dirname(__DIR__) . '/layouts/navbar.php';
