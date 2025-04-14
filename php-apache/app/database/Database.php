e5258af7324dca02a42131135d23a3beb0c2910f
= <LHP6.arazzok@gmail.com>
2025-04-14 23:00:41 +0200

    udpate: turn database into abstract class

--- php-apache/app/database/Database.php
+++ php-apache/app/database/Database.php
@@ -4,11 +4,14 @@ namespace database;
 
 use PDO;
 
-class Database
+abstract class Database
 {
     protected string $host;
+    protected int $port;
     protected string $database;
     protected string $username;
     protected string $password;
     protected PDO $connection;
+
+    abstract public function getConnection(): PDO;
 }
