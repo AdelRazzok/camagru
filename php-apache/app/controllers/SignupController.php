34a6ecc95c6ae5487c9badebbe16bd9c7f2cc10b
= <LHP6.arazzok@gmail.com>
2025-04-25 10:44:31 +0200

    update: call token generation service in sign up controller

--- php-apache/app/controllers/SignupController.php
+++ php-apache/app/controllers/SignupController.php
@@ -3,19 +3,25 @@
 namespace controllers;
 
 use http\Response;
-use repositories\SQLUserRepository;
 use database\Sqlite;
+use models\enums\TokenType;
+use repositories\SQLUserRepository;
+use repositories\SQLTokenRepository;
 use services\UserService;
+use services\TokenService;
 
 class SignupController
 {
     private UserService $userService;
+    private TokenService $tokenService;
 
     public function __construct()
     {
         $db = new Sqlite(dirname(__DIR__) . '/database/SQLite/camagru.db');
-        $repository = new SQLUserRepository($db->getConnection());
-        $this->userService = new UserService($repository);
+        $userRepository = new SQLUserRepository($db->getConnection());
+        $tokenRepository = new SQLTokenRepository($db->getConnection());
+        $this->userService = new UserService($userRepository);
+        $this->tokenService = new TokenService($tokenRepository);
     }
 
     public function index()
@@ -31,20 +37,28 @@ class SignupController
 
     public function store()
     {
-        $result = $this->userService->createUser(
+        $userResult = $this->userService->createUser(
             $_POST['email'],
             $_POST['username'],
             $_POST['password']
         );
 
-        if (!$result['success']) {
-            $_SESSION['errors'] = $result['errors'];
-            $_SESSION['old'] = $result['data'];
+        if (!$userResult['success']) {
+            $_SESSION['errors'] = $userResult['errors'];
+            $_SESSION['old'] = $userResult['data'];
 
             $response = new Response(Response::HTTP_SEE_OTHER);
             $response->addHeader('Location', '/signup');
             $response->send();
             exit;
+        }
+
+        $tokenResult = $this->tokenService->generateToken(
+            $userResult['user']->getId(),
+            TokenType::EmailVerification
+        );
+
+        if ($tokenResult['success']) {
         }
 
         $response = new Response(Response::HTTP_SEE_OTHER);
