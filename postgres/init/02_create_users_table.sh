d88d6c52e17e137f72d4470a6ff37e5ca0305b6e
AdelRazzok <lhp6.arazzok@gmail.com>
2025-08-03 12:02:15 +0200

    update: formatting

--- postgres/init/02_create_users_table.sh
+++ postgres/init/02_create_users_table.sh
@@ -3,14 +3,14 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB_NAME" <<-EOSQL
-    CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        email TEXT NOT NULL,
-        username TEXT NOT NULL,
-        password TEXT NOT NULL,
-        email_verified BOOLEAN NOT NULL DEFAULT FALSE,
-        email_notif_on_comment BOOLEAN NOT NULL DEFAULT TRUE,
-        created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-        updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
-    );
+  CREATE TABLE IF NOT EXISTS users (
+    id                      SERIAL PRIMARY KEY,
+    email                   TEXT NOT NULL,
+    username                TEXT NOT NULL,
+    password                TEXT NOT NULL,
+    email_verified BOOLEAN  NOT NULL DEFAULT FALSE,
+    email_notif_on_comment  BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at              TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    updated_at              TIMESTAMP WITHOUT TIME ZONE NOT NULL
+  );
 EOSQL
--- postgres/init/03_create_tokens_table.sh
+++ postgres/init/03_create_tokens_table.sh
@@ -3,14 +3,14 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB_NAME" <<-EOSQL
-    CREATE TABLE IF NOT EXISTS tokens (
-        id SERIAL PRIMARY KEY,
-        user_id INTEGER NOT NULL,
-        token TEXT NOT NULL,
-        type TEXT NOT NULL,
-        expires_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-        created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-        updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-    );
+  CREATE TABLE IF NOT EXISTS tokens (
+    id          SERIAL PRIMARY KEY,
+    user_id     INTEGER NOT NULL,
+    token       TEXT NOT NULL,
+    type        TEXT NOT NULL,
+    expires_at  TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    created_at  TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    updated_at  TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+  );
 EOSQL
