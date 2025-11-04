7ebced21d2ada830512afa478f89eaffdafea886
AdelRazzok <lhp6.arazzok@gmail.com>
2025-11-04 14:58:51 +0100

    add: test user to save time

--- postgres/init/02_create_users_table.sh
+++ postgres/init/02_create_users_table.sh
@@ -14,3 +14,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB_NAME" <<-EOSQL
     updated_at              TIMESTAMP WITHOUT TIME ZONE NOT NULL
   );
 EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB_NAME" <<'EOSQL'
+  INSERT INTO users (email, username, password, email_verified, email_notif_on_comment, created_at, updated_at)
+    VALUES ('test@mail.com', 'test', '$2y$10$ncxHnUY5K89mxZ9WYe6Pk.D8wEWz/G0szrPirCiZOQPn/BmX3DggO', TRUE, TRUE, NOW(), NOW());
+EOSQL
